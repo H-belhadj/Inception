675fe0823dad4eb5e3fdb9ac310c3898928718c3
Haitam Belhadj <steankir.haitam@gmail.com>
2024-12-20 19:14:59 -0500

    v1.0

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+
 service mariadb start
+
 sleep 1
+
 mariadb << MYSQL_SCRIPT
 CREATE DATABASE IF NOT EXISTS \`${DB_NAME}\`;
 CREATE USER IF NOT EXISTS \`${DB_USER}\`@'localhost' IDENTIFIED BY '${DB_PASS}';
 GRANT ALL PRIVILEGES ON \`${DB_NAME}\`.* TO \`${DB_USER}\`@'%' IDENTIFIED BY '${DB_PASS}';
 MYSQL_SCRIPT
+
 service mariadb stop
+
 mysqld_safe
