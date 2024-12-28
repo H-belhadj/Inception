b33753d4e72ca5f783c4fc7cd2335f784ca79254
Haitam Belhadj <steankir.haitam@gmail.com>
2024-12-28 17:20:53 -0500

    done

--- srcs/requirements/wordpress/tools/script.sh
+++ srcs/requirements/wordpress/tools/script.sh
@@ -18,12 +18,15 @@ if [ -f /var/www/html /wp-config.php ]
     wp config set DB_PASSWORD $DB_PASS --allow-root
     wp config set DB_HOST 'mariadb:3306' --allow-root
 
-    wp core install --url=$DOMAIN_NAME --title=inception \
+    wp core install --url=$DOMAIN_NAME --title=$TITLE \
         --admin_user=$WP_ADMIN --admin_password=$WP_ADMINPASS \
         --admin_email=$WP_ADMINEMAIL --allow-root
     wp user create $WP_USER $WP_USEREMAIL --role=author --user_pass=$WP_USERPASS \
         --allow-root
+    wp theme install twentytwentytwo --activate --allow-root
+    wp option set comment_previously_approved 0 --allow-root
 
+    chown -R www-data:www-data /var/www/html/
 fi
 
 mkdir -p /run/php/
